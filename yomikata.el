dca756be48a3cf198f94503e0f271be40d2536dc
Melissa Boiko <melissa@namakajiri.net>
2026-03-04 15:04:58 +0100

    clearer commentary

--- yomikata.el
+++ yomikata.el
@@ -15,38 +15,41 @@
 ;; readings for the kanji characters, using MECAB+Unidic (an external
 ;; tool).
 ;;
-;; To annotate the text, run `yomikata-region', `yomikata-buffer', or
-;; `yomikata-at-point'.  The readings will be saved as help-echo text
-;; properties.  To see them, hover the mouse pointer, or by call
-;; `yomikata-at-point' again at any word.
+;; To annotate text, run `yomikata-region', `yomikata-buffer', or
+;; `yomikata-at-point'.  This will not put visible glosses
+;; (furigana/rubi) onto the buffer, but instead add them as
+;; interactive tooltips.  To see the reading of a kanji, hover the
+;; mouse pointer over it, or call `yomikata-at-point' again at any
+;; word.
 ;;
-;; You need MECAB installed with the Unidic dictionary (alternative
-;; dictionaries are planned but not yet supported).  On Debian you can
-;; get them with:
+;; You will need MECAB installed with the Unidic dictionary
+;; (alternative dictionaries are planned but not yet supported).
+;; On Debian you can get them with:
 ;;
 ;;    apt install mecab unidic-mecab
 ;;
 ;; Note that unidic-mecab is several gigabytes in size.
 ;;
-;; The annotations are saved in package-specific overlays.  To clear
-;; them, use `yomikata-clear-tooltips-region' or
+;; The annotations are saved in the `help-echo' text property of a
+;; package-specific overlay.  To clear them, use
+;; `yomikata-clear-tooltips-region' or
 ;; `yomikata-clear-tooltips-buffer'.
 ;;
 ;; Annotated text is underlined by default.  To change this, customize
 ;; `yomikata-tooltip-available-face'.
 ;;
 ;;
-;; Automatic morphological analysis and reading inference are
-;; imperfect processes and may make errors.  This software is
+;; This software currently doesn't understand Japanese words broken
+;; between lines.  You might want to use soft line breaks for longer
+;; text.
+;;
+;; Automatic morphological analysis and kanji reading inference are
+;; imperfect processes, and may make errors.  This software is
 ;; deterministic, and its errors are consistent and predictable.  The
 ;; inference is 100% offline, open source, and private.  No LLMs or
 ;; so-called “generative AI” are used at any point.  None of your
 ;; data is sent anywhere or used to train anything.  No coal plants
 ;; had to be built to train datafiles used by this software.
-;;
-;; This software currently doesn't understand Japanese words broken
-;; between lines.  You might want to use soft line breaks for longer
-;; text.
 
 
 ;;; Code:
